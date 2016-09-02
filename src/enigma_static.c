af13c776031c2b4b36066dd01c9281bd0e97e7d8
Travis La Marr <exiva@exiva.net>
2016-09-02 02:57:48 -0400

    Cleaned up code. Improved Config saving.

--- src/enigma_static.c
+++ src/enigma_static.c
@@ -1,28 +1,21 @@
 #include <pebble.h>
 
-static Window *window;
-
-//Setup Layers.
-static Layer *year_layer;
-static Layer *time_layer;
-static Layer *date_layer;
-static Layer *top_random;
-static Layer *bottom_random;
-
-static GSize textSize;
-static bool is_obstructed;
-
-enum {
-  bg = 0,
-  fg = 1
-};
-
-typedef struct {
+typedef struct __attribute__((__packed__)) {
   int32_t fg_color;
   int32_t bg_color;
-} colorSettings;
+} settings;
 
-colorSettings *settings;
+struct app {
+  Window *window;
+  Layer *year_layer;
+  Layer *time_layer;
+  Layer *date_layer;
+  Layer *top_random;
+  Layer *bottom_random;
+  GSize textSize;
+  bool is_obstructed;
+  settings config;
+} *app;
 
 #define FONT_PADDING 6
 #define NUMBER_GAP 10
@@ -38,13 +31,13 @@ static int getRandNumber() {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   if (units_changed & MINUTE_UNIT) {
-    layer_mark_dirty(time_layer);
+    layer_mark_dirty(app->time_layer);
   }
   if (units_changed & DAY_UNIT) {
-    layer_mark_dirty(date_layer);
+    layer_mark_dirty(app->date_layer);
   }
   if (units_changed & YEAR_UNIT) {
-    layer_mark_dirty(year_layer);
+    layer_mark_dirty(app->year_layer);
   }
 }
 
@@ -53,11 +46,11 @@ static void draw_round_numbers(GContext *ctx, GRect bounds, int height) {
   char tmp[2];
   snprintf(tmp, 2, "%d", getRandNumber());
   graphics_draw_text(ctx, tmp, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
-        GRect(-textSize.w/2, height, textSize.w, bounds.size.h),
+        GRect(-app->textSize.w/2, height, app->textSize.w, bounds.size.h),
         GTextOverflowModeFill, GTextAlignmentCenter, NULL);
   snprintf(tmp, 2, "%d", getRandNumber());
   graphics_draw_text(ctx, tmp, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
-        GRect(bounds.size.w-textSize.w/2, height, textSize.w, bounds.size.h),
+        GRect(bounds.size.w-app->textSize.w/2, height, app->textSize.w, bounds.size.h),
         GTextOverflowModeFill, GTextAlignmentCenter, NULL);
 }
 #endif
@@ -66,8 +59,8 @@ static void update_time(Layer *layer, GContext *ctx) {
   // APP_LOG(APP_LOG_LEVEL_INFO, "Drawing Time Layer.");
   GRect bounds = layer_get_bounds(layer);
 
-  graphics_context_set_text_color(ctx, PBL_IF_COLOR_ELSE(GColorFromHEX(settings->fg_color), GColorWhite));
-  graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorFromHEX(settings->bg_color), GColorLightGray));
+  graphics_context_set_text_color(ctx, PBL_IF_COLOR_ELSE(GColorFromHEX(app->config.fg_color), GColorWhite));
+  graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorFromHEX(app->config.bg_color), GColorLightGray));
   graphics_fill_rect(ctx, GRect(0, 0, bounds.size.w, bounds.size.h-1),0,GCornerNone);
 
   time_t temp = time(NULL);
@@ -80,7 +73,7 @@ static void update_time(Layer *layer, GContext *ctx) {
     s_buffer[i][0] = s_buffer[0][n];
     s_buffer[i][1] = '\n';
     graphics_draw_text(ctx, s_buffer[i], fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
-    			GRect((NUMBER_GAP * n) + (textSize.w * n) + FONT_PADDING + PADDING, -7, textSize.w, bounds.size.h),
+    			GRect((NUMBER_GAP * n) + (app->textSize.w * n) + FONT_PADDING + PADDING, -7, app->textSize.w, bounds.size.h),
     			GTextOverflowModeFill, GTextAlignmentCenter, NULL);
     n++;
   }
@@ -105,7 +98,7 @@ static void update_date(Layer *layer, GContext *ctx) {
     s_buffer[i][0] = s_buffer[0][n];
     s_buffer[i][1] = '\n';
     graphics_draw_text(ctx, s_buffer[i], fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
-    			GRect((NUMBER_GAP * n) + (textSize.w * n) + FONT_PADDING + PADDING, 0, textSize.w, bounds.size.h),
+    			GRect((NUMBER_GAP * n) + (app->textSize.w * n) + FONT_PADDING + PADDING, 0, app->textSize.w, bounds.size.h),
     			GTextOverflowModeFill, GTextAlignmentCenter, NULL);
     n++;
   }
@@ -129,7 +122,7 @@ static void update_year(Layer *layer, GContext *ctx) {
     s_buffer[i][0] = s_buffer[0][n];
     s_buffer[i][1] = '\n';
     graphics_draw_text(ctx, s_buffer[i], fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
-    			GRect((NUMBER_GAP * n) + (textSize.w * n) + FONT_PADDING  + PADDING, 0, textSize.w, bounds.size.h),
+    			GRect((NUMBER_GAP * n) + (app->textSize.w * n) + FONT_PADDING  + PADDING, 0, app->textSize.w, bounds.size.h),
     			GTextOverflowModeFill, GTextAlignmentCenter, NULL);
     n++;
   }
@@ -148,47 +141,47 @@ static void generate_random(Layer *layer, GContext *ctx) {
   for (size_t i = 0; i < 4; i++) {
     snprintf(tmp, 2, "%d", getRandNumber());
     graphics_draw_text(ctx, tmp, fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
-    			GRect((NUMBER_GAP * i) + (textSize.w * i) + FONT_PADDING + PADDING, 0, textSize.w, bounds.size.h),
+    			GRect((NUMBER_GAP * i) + (app->textSize.w * i) + FONT_PADDING + PADDING, 0, app->textSize.w, bounds.size.h),
     			GTextOverflowModeFill, GTextAlignmentCenter, NULL);
   }
 }
 
 static void obstructed_will_change(GRect final_unobstructed_screen_area, void *context) {
-  GRect year_frame = layer_get_frame(year_layer);
-  GRect time_frame = layer_get_frame(time_layer);
-  GRect date_frame = layer_get_frame(date_layer);
-  GRect full_bounds = layer_get_bounds(window_get_root_layer(window));
-
+  GRect year_frame = layer_get_frame(app->year_layer);
+  GRect time_frame = layer_get_frame(app->time_layer);
+  GRect date_frame = layer_get_frame(app->date_layer);
+  GRect full_bounds = layer_get_bounds(window_get_root_layer(app->window));
+  
   if (!grect_equal(&full_bounds, &final_unobstructed_screen_area)) {
     // Screen is about to become obstructed, hide the date
-    layer_set_hidden(top_random, true);
-    layer_set_hidden(bottom_random, true);
-
+    layer_set_hidden(app->top_random, true);
+    layer_set_hidden(app->bottom_random, true);
+    
     year_frame.origin.y = -10;
-    layer_set_frame(year_layer, year_frame);
+    layer_set_frame(app->year_layer, year_frame);
+    
+    time_frame.origin.y = app->textSize.h-3;
+    layer_set_frame(app->time_layer, time_frame);
 
-    time_frame.origin.y = textSize.h-3;
-    layer_set_frame(time_layer, time_frame);
-
-    date_frame.origin.y = textSize.h*2-10;
-    layer_set_frame(date_layer, date_frame);
+    date_frame.origin.y = app->textSize.h*2-10;
+    layer_set_frame(app->date_layer, date_frame);
   } else {
     year_frame.origin.y = 12;
-    layer_set_frame(year_layer, year_frame);
-
+    layer_set_frame(app->year_layer, year_frame);
+    
     time_frame.origin.y = 63;
-    layer_set_frame(time_layer, time_frame);
+    layer_set_frame(app->time_layer, time_frame);
 
     date_frame.origin.y = 100;
-    layer_set_frame(date_layer, date_frame);
-
-    layer_set_hidden(top_random, false);
-    layer_set_hidden(bottom_random, false);
+    layer_set_frame(app->date_layer, date_frame);
+    
+    layer_set_hidden(app->top_random, false);
+    layer_set_hidden(app->bottom_random, false);
   }
 }
 
 static void window_load(Window *window) {
-  textSize = graphics_text_layout_get_content_size("0",
+  app->textSize = graphics_text_layout_get_content_size("0",
                 fonts_get_system_font(FONT_KEY_LECO_42_NUMBERS),
                 GRect(0, 0, 50, 42), GTextOverflowModeFill,
                 GTextAlignmentCenter);
@@ -196,73 +189,70 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
   window_set_background_color(window, GColorBlack);
-
+  
   GRect unobstructed_bounds = layer_get_unobstructed_bounds(window_layer);
-  is_obstructed = !grect_equal(&bounds, &unobstructed_bounds);
-
+  app->is_obstructed = !grect_equal(&bounds, &unobstructed_bounds);
+  
   UnobstructedAreaHandlers unobstructed_handlers = {
     .will_change = obstructed_will_change,
   };
 
   unobstructed_area_service_subscribe(unobstructed_handlers, NULL);
-
+  
   //draw random numbers
-  top_random = layer_create(GRect(0, PBL_IF_RECT_ELSE(-32, -26), bounds.size.w, textSize.h));
-  layer_add_child(window_layer, top_random);
-  layer_set_update_proc(top_random, generate_random);
+  app->top_random = layer_create(GRect(0, PBL_IF_RECT_ELSE(-32, -26), bounds.size.w, app->textSize.h));
+  layer_add_child(window_layer, app->top_random);
+  layer_set_update_proc(app->top_random, generate_random);
 
-  bottom_random = layer_create(GRect(0, PBL_IF_RECT_ELSE(144, 150), bounds.size.w, textSize.h));
-  layer_add_child(window_layer, bottom_random);
-  layer_set_update_proc(bottom_random, generate_random);
+  app->bottom_random = layer_create(GRect(0, PBL_IF_RECT_ELSE(144, 150), bounds.size.w, app->textSize.h));
+  layer_add_child(window_layer, app->bottom_random);
+  layer_set_update_proc(app->bottom_random, generate_random);
 
-  year_layer = layer_create(GRect(0, PBL_IF_RECT_ELSE(is_obstructed ? -10 : 12, 18), bounds.size.w, textSize.h));
-  layer_add_child(window_layer, year_layer);
-  layer_set_update_proc(year_layer, update_year);
+  app->year_layer = layer_create(GRect(0, PBL_IF_RECT_ELSE(app->is_obstructed ? -10 : 12, 18), bounds.size.w, app->textSize.h));
+  layer_add_child(window_layer, app->year_layer);
+  layer_set_update_proc(app->year_layer, update_year);
 
-  time_layer = layer_create(GRect(0, PBL_IF_RECT_ELSE(is_obstructed ? textSize.h-3 : 63, 69), bounds.size.w, textSize.h));
-  layer_add_child(window_layer,time_layer);
-  layer_set_update_proc(time_layer, update_time);
+  app->time_layer = layer_create(GRect(0, PBL_IF_RECT_ELSE(app->is_obstructed ? app->textSize.h-3 : 63, 69), bounds.size.w, app->textSize.h));
+  layer_add_child(window_layer, app->time_layer);
+  layer_set_update_proc(app->time_layer, update_time);
 
-  date_layer = layer_create(GRect(0, PBL_IF_RECT_ELSE(is_obstructed ? textSize.h*2-10 : 100, 106), bounds.size.w, textSize.h));
-  layer_add_child(window_layer, date_layer);
-  layer_set_update_proc(date_layer, update_date);
+  app->date_layer = layer_create(GRect(0, PBL_IF_RECT_ELSE(app->is_obstructed ? app->textSize.h*2-10 : 100, 106), bounds.size.w, app->textSize.h));
+  layer_add_child(window_layer, app->date_layer);
+  layer_set_update_proc(app->date_layer, update_date);
 
-  if (is_obstructed) {
-    layer_set_hidden(top_random, true);
-    layer_set_hidden(bottom_random, true);
+  if (app->is_obstructed) {
+    layer_set_hidden(app->top_random, true);
+    layer_set_hidden(app->bottom_random, true);
   }
 }
 
 static void window_unload(Window *window) {
-  layer_destroy(top_random);
-  layer_destroy(bottom_random);
-  layer_destroy(year_layer);
-  layer_destroy(time_layer);
-  layer_destroy(date_layer);
+  layer_destroy(app->top_random);
+  layer_destroy(app->bottom_random);
+  layer_destroy(app->year_layer);
+  layer_destroy(app->time_layer);
+  layer_destroy(app->date_layer);
 }
 
 static void inbox_received_handler(DictionaryIterator *iter, void *context) {
-  if (!dict_find(iter, bg)) return;
-  settings->bg_color = dict_find(iter, bg)->value->int32;
-  settings->fg_color = dict_find(iter, fg)->value->int32;
-  persist_write_int(bg, settings->bg_color);
-  persist_write_int(fg, settings->fg_color);
-  layer_mark_dirty(time_layer);
+  if (!dict_find(iter, MESSAGE_KEY_bg)) return;
+  app->config.bg_color = dict_find(iter, MESSAGE_KEY_bg)->value->int32;
+  app->config.fg_color = dict_find(iter, MESSAGE_KEY_fg)->value->int32;
+  layer_mark_dirty(app->time_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  settings = calloc(1, sizeof(colorSettings));
+  app = calloc(1, sizeof(*app));
 
-  if (persist_exists(bg)) {
-    settings->bg_color = persist_read_int(bg);
-    settings->fg_color = persist_read_int(fg);
-  } else {
-    settings->bg_color = 16755200;
-    settings->fg_color = 16777215;
+  app->window = window_create();
+  app->config.fg_color = 16777215;
+  app->config.bg_color = 16755200;
+
+  if (persist_exists(MESSAGE_KEY_config)) {
+    persist_read_data(MESSAGE_KEY_config, &app->config, sizeof(app->config));
   }
 
-  window_set_window_handlers(window, (WindowHandlers) {
+  window_set_window_handlers(app->window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
@@ -270,7 +260,7 @@ static void init(void) {
   srand(time(NULL)); //seed random number generator
   //subscribe to multiple tick events to update layers accordingly.
   tick_timer_service_subscribe(MINUTE_UNIT | DAY_UNIT | YEAR_UNIT, tick_handler);
-  window_stack_push(window, false);
+  window_stack_push(app->window, false);
 
   app_message_register_inbox_received(inbox_received_handler);
   app_message_open(128, 0);
@@ -278,8 +268,9 @@ static void init(void) {
 
 static void deinit(void) {
   tick_timer_service_unsubscribe();
-  free(settings);
-  window_destroy(window);
+  persist_write_data(MESSAGE_KEY_config, &app->config, sizeof(app->config));
+  if (app) free(app);
+  window_destroy(app->window);
 }
 
 int main(void) {
